14b0968218a135e0327f0754701f8618b5a24a89
Samim Aktar <samimaktar0773@gmail.com>
2021-10-18 23:49:10 +0530

    Done UC1 Check Equality ot Two Line

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -3,32 +3,32 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 class CalculateLength {
-	double length ;
+	double length;
 
 	public CalculateLength(int x1, int y1, int x2, int y2) {
-		length = Math.sqrt(Math.pow((x2 - x1),2) + Math.pow((y2 - y1 ),2));
+		length = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
 	}
-	
+
 }
 
-class CalculateEqualityOfTwoline{
+class CalculateEqualityOfTwoline {
 	double length2;
-	public CalculateEqualityOfTwoline(int x1,int y1,int x2,int y2) {
-		length2 = Math.sqrt(Math.pow((x2 - x1),2) + Math.pow((y2 - y1 ),2));
-	}
-	
-}
 
+	public CalculateEqualityOfTwoline(int x1, int y1, int x2, int y2) {
+		length2 = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+	}
+
+}
 
 public class LineComparison {
 	public static void main(String[] args) {
 		System.out.println("Welcome to line comparison computation program");
-		
-		int x1,x2,y1,y2;
-		int X1,Y1,X2,Y2;
-		
+
+		int x1, x2, y1, y2;
+		int X1, Y1, X2, Y2;
+
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("Enter first line x1 point: ");
 		x1 = scanner.nextInt();
 		System.out.println("Enter first line  y1 point: ");
@@ -37,10 +37,26 @@ public class LineComparison {
 		x2 = scanner.nextInt();
 		System.out.println("Enter first line  y2 point: ");
 		y2 = scanner.nextInt();
+
+		System.out.println("Enter second line x1 point: ");
+		X1 = scanner.nextInt();
+		System.out.println("Enter second line  y1 point: ");
+		Y1 = scanner.nextInt();
+		System.out.println("Enter second line x2 point: ");
+		X2 = scanner.nextInt();
+		System.out.println("Enter second line  y2 point: ");
+		Y2 = scanner.nextInt();
+
+		CalculateLength cl = new CalculateLength(x1, y1, x2, y2);
+		CalculateEqualityOfTwoline cetl = new CalculateEqualityOfTwoline(X1, Y1, X2, Y2);
+
+		System.out.println("length : " + cl.length);
 		
-		CalculateLength cl = new CalculateLength(x1,y1,x2,y2);
-		System.out.println("length : "+cl.length);
-		 
+		Double lineOneDouble = cl.length;
+		Double lineTwoDouble = cetl.length2;
+
+		System.out.println("Equality of two line: " + lineOneDouble.equals(lineTwoDouble));
+
 	}
 
 }
